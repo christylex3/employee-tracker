25e898ba7277b66eb81827f2f977fa0d9130a728
Christy Le <christylex3@gmail.com>
2022-07-30 15:00:06 -0400

    Added foreign key to manager id to reference to the id in the same table

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,17 @@
 DROP DATABASE IF EXISTS business_db;
 CREATE DATABASE business_db;
 
+-- Selects database to be used
 USE business_db;
 
+-- Creates the table called department
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
+-- Creates the table called role
 CREATE TABLE role (
     id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
@@ -18,10 +21,14 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+-- Creates the table called employee
 CREATE TABLE employee (
     id INT NOT NULL,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT NOT NULL,
+    manager_id INT NOT NULL REFERENCES employee(id),
+    PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id)
+
 );
